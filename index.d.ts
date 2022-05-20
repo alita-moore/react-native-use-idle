c8dfeed3bbfeee33ecf1a213e081e6d933019236
Alita Moore <github@wishonce.com>
2022-05-20 00:58:48 -0500

    declare module and add docstring

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,37 @@
-export function useIdle(): boolean;
-export function IdleProvider(props: { timeForInactivity?: number }): JSX.Element;
+declare module 'react-native-use-idle' {
+
+    /**
+     * @returns (boolean) a stateful boolean for isIdle, this requires that IdleProvider be implemented at the top level
+     * ```ts
+     * const Component = () => {
+     *     const isIdle = useIdle();
+     *     useEffect(() => {
+     *          if (!isIdle) console.log("not idle");
+     *          console.log("is idle");
+     *     },[])
+     * }
+     * ```
+     */
+    export function useIdle(): boolean;
+
+    /**
+     * Top level provider container
+     * @param timeForInactivity (number): time in milliseconds before idle; default is 30000 or 30 seconds
+     *
+     * ```ts
+     * const App = () => {
+     *     return (
+     *         <IdleProvider>
+     *             {...your app}
+     *          </IdleProvider>
+     *     )
+     * }
+     * ```
+     *
+     * This utilizes the pan handlers that are provided by react native. This acts as a standard listener
+     * so that means there are many events. Therefore, the root code clears and re-instantiates a set timeout
+     * every time an action is performed. This is unlikely to cause issues with your app.
+     * */
+    export function IdleProvider(props: { timeForInactivity?: number }): JSX.Element;
+}
+
--- src/index.tsx
+++ src/index.tsx
@@ -12,24 +12,7 @@ import {
 
 const IsIdleContext = createContext(false);
 
-/**
- * Top level provider container
- * @param timeForInactivity (number): time in milliseconds before idle; default is 30000 or 30 seconds
- *
- * ```ts
- * const App = () => {
- *     return (
- *         <IdleProvider>
- *             {...your app}
- *          </IdleProvider>
- *     )
- * }
- * ```
- *
- * This utilizes the pan handlers that are provided by react native. This acts as a standard listener
- * so that means there are many events. Therefore, the root code clears and re-instantiates a set timeout
- * every time an action is performed. This is unlikely to cause issues with your app.
- * */
+
 export const IdleProvider = ({
   timeForInactivity = 30000,
   children,
@@ -73,18 +56,7 @@ export const IdleProvider = ({
   );
 };
 
-/** *
- * @returns (boolean) a stateful boolean for isIdle, this requires that IdleProvider be implemented at the top level
- * ```ts
- * const Component = () => {
- *     const isIdle = useIdle();
- *     useEffect(() => {
- *          if (!isIdle) console.log("not idle");
- *          console.log("is idle");
- *     },[])
- * }
- * ```
- */
+/
 export const useIdle = () => {
   return useContext(IsIdleContext);
 };
